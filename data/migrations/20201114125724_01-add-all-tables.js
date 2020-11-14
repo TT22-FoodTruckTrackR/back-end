3c485936f5fc0b351f922b9c3e7de862ede7574f
Suzanne Cabral <suzannecabral@live.com>
2020-11-14 13:23:57 -0800

    Added schema notes

--- data/migrations/20201114125724_01-add-all-tables.js
+++ data/migrations/20201114125724_01-add-all-tables.js
@@ -1,6 +1,15 @@
+//operators
+//diners
+//trucks
+//operators_trucks
+//diners_trucks
+
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('operators', tbl=>{
+    })
+    ;
 };
 
 exports.down = function(knex) {
