035628d6ea099f57ce88c3ab2a049ba087e9b82b
Suzanne Cabral <suzannecabral@live.com>
2020-11-14 12:51:54 -0800

    added trucks router structure

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const logger = require('./middleware/logger');
 //require routers
+const trucksRouter = require('./trucks/trucksRouter');
 
 
 //new server
@@ -10,12 +11,18 @@ const server = express();
 //use middleware
 server.use(express.json());
 server.use(logger);
+
 //use routers
+server.use('/api/trucks', trucksRouter);
 
 
 //default response
 server.get('/', (req,res)=>{
   res.status(200).json({message:"The server is running, better go catch it"});
+});
+
+server.get('/api', (req,res)=>{
+  res.status(200).json({message:"Welcome to the api, please choose an endpoint. See backend documentation for server routes."});
 });
 
 //export server
