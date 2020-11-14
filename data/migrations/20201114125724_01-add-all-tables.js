9d27dc59560bdd0921a108dd595827b5a82ef879
Suzanne Cabral <suzannecabral@live.com>
2020-11-14 13:50:41 -0800

    Added operators table

--- data/migrations/20201114125724_01-add-all-tables.js
+++ data/migrations/20201114125724_01-add-all-tables.js
@@ -8,10 +8,30 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('operators', tbl=>{
+      tbl.increments()
+      ;
+      tbl.text('username',50)
+        .notNullable()
+        .unique()
+      ;
+      tbl.text('password',50)
+        .notNullable();
+      ; 
+      tbl.text('email')
+        .notNullable()
+        .unique();
+      ;
+      tbl.text('role')
+        .notNullable()
+      ;
+      tbl.integer('location')
+        .unsigned()
+      ;
     })
     ;
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('operators');
 };
