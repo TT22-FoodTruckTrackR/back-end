b0054a60bb44d6cf93fb506acb26a3e0fdd2d0d4
Suzanne Cabral <suzannecabral@live.com>
2020-11-17 10:51:07 -0800

    Rewriting router and db methods, progress save

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -11,7 +11,6 @@ const {jwtSecret} = require('./secrets.js');
 // ENDPOINTS
 //-------------------------------------------
 
-
 /*  
 user
 {
@@ -23,61 +22,157 @@ user
 }
 */
 
-//TESTING: GET ALL USERS
-//---------
-//usage: 
-//GET /api/auth/operators
-//GET /api/auth/diners
-router.get('/:role', (req, res)=>{
-  const role=req.params;
 
-  Users.getAll(role)
-    .then(users=>{
-      res.status(200).json(users)
+// //---------
+// //TESTING: GET ALL USERS
+// //---------
+
+// //GET /api/auth/operators
+//---------------------------------
+router.get('/operators', (req, res, next)=>{
+
+  Users.getAllOperators
+    .then(data =>{
+      res.status(200).json(data)
     })
     .catch(err=>{
-      console.log(err)
-      res.status(500).json({message:'Error getting users', error:err.message, name:err.name});
+      console.log(err);
+      res.status(500).json({message:'Server error getting operators', error:err})
     })
-  // res.status(200).json({message:'Test successful'});
+
+  // try{ 
+  //   Users.getAllOperators
+  // }catch(err){
+  //   console.log(err);
+  //   res.json(500).json({message:'Server error returning operators list', error:err})
+  // }
+  // next();
 });
 
+
+// //GET /api/auth/diners
+//---------------------------------
+router.get('/diners', (req, res, next)=>{
+  next();
+});
+
+
+
+//---------
+//REGISTER
+//---------
+
+//POST /api/auth/register/operators
+//---------------------------------
+router.post('/register/operators', (req, res, next)=>{
+  next();
+});
+
+
+
+//POST /api/auth/register/diners
+//---------------------------------
+router.post('/register/diners', (req, res, next)=>{
+  next();
+});
+
+
+
+
+//---------
+//LOGIN
+//---------
+
+//POST /api/auth/login/operators
+//---------------------------------
+router.post('/login/operators', (req, res, next)=>{
+  next();
+});
+
+
+
+
+
+//POST /api/auth/login/diners
+//---------------------------------
+router.post('/login/diners', (req, res, next)=>{
+  next();
+});
+
+
+
+
+
+
+// ////OLD--------------------------------------------
+//----------------------------------------------------
+//----------------------------------------------------
+/*  
+user
+{
+  username: string, unique, required
+  password: string, required
+  email: string, unique, required
+  role: diner or operator, required
+  location: integer, optional
+}
+*/
+// ////OLD--------------------------------------------
+// //TESTING: GET ALL USERS
+// //---------
+// //usage: 
+// //GET /api/auth/operators
+// //GET /api/auth/diners
+// router.get('/:role', (req, res)=>{
+//   const role=req.params;
+
+//   Users.getAll(role)
+//     .then(users=>{
+//       res.status(200).json(users)
+//     })
+//     .catch(err=>{
+//       console.log(err)
+//       res.status(500).json({message:'Error getting users', error:err.message, name:err.name});
+//     })
+//   // res.status(200).json({message:'Test successful'});
+// });
+// ////OLD--------------------------------------------
 
 //TESTING: GET USER BY NAME
 //---------
 //usage: 
 //GET /api/auth/find/operators
 //GET /api/auth/find/diners
-
-router.get('/diners/:username', (req,res)=>{
-  const username = req.params;
-
-
-  Users.findByName(username, 'diners')
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err=>{
-      console.log(err);
-      res.status(500).json({message:'Error getting user', error:err.message, name:err.name});
-    });
-});
+// ////OLD--------------------------------------------
+// router.get('/diners/:username', (req,res)=>{
+//   const username = req.params;
 
 
-router.get('/operators/:username', (req,res)=>{
-  const username = req.params;
+//   Users.findByName(username, 'diners')
+//     .then(user => {
+//       res.status(200).json(user);
+//     })
+//     .catch(err=>{
+//       console.log(err);
+//       res.status(500).json({message:'Error getting user', error:err.message, name:err.name});
+//     });
+// });
 
 
-  Users.findByName(username, 'operators')
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err=>{
-      console.log(err);
-      res.status(500).json({message:'Error getting user', error:err.message, name:err.name});
-    });
-});
+// router.get('/operators/:username', (req,res)=>{
+//   const username = req.params;
 
+
+//   Users.findByName(username, 'operators')
+//     .then(user => {
+//       res.status(200).json(user);
+//     })
+//     .catch(err=>{
+//       console.log(err);
+//       res.status(500).json({message:'Error getting user', error:err.message, name:err.name});
+//     });
+// });
+// ////OLD--------------------------------------------
 
 //REGISTER
 //---------
@@ -85,25 +180,31 @@ router.get('/operators/:username', (req,res)=>{
 //POST /api/auth/register/operators
 //POST /api/auth/register/diners
 
-router.post('/register/:role',(req,res)=>{
-  const role = req.params;
-  const newUser = req.body;
-  
-  console.log(req.params);
-  //theoretical env
-  const rounds = process.env.BCRYPT_ROUNDS || 8;
-  const hash = bcrypt.hashSync(newUser.password, rounds);
-  newUser.password = hash;
 
-  Users.addNew(newUser, role)
-    .then(user=>{
-      res.status(201).json({message:'User created successfully',user})
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({message:'Error creating user', error:err.message, name:err.name});
-    });
-  })
-});
+
+
+////OLD--------------------------------------------
+
+// router.post('/register/:role',(req,res)=>{
+//   const role = req.params;
+//   const newUser = req.body;
+  
+//   console.log(req.params);
+//   //theoretical env
+//   const rounds = process.env.BCRYPT_ROUNDS || 8;
+//   const hash = bcrypt.hashSync(newUser.password, rounds);
+//   newUser.password = hash;
+
+//   Users.addNew(newUser, role)
+//     .then(user=>{
+//       res.status(201).json({message:'User created successfully',user})
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({message:'Error creating user', error:err.message, name:err.name});
+//     });
+//   })
+// });
+////OLD--------------------------------------------
 
 
 //LOGIN
@@ -112,25 +213,36 @@ router.post('/register/:role',(req,res)=>{
 //POST /api/auth/login/operators
 //POST /api/auth/login/diners
 
-router.post('/login/:role',(req,res)=>{
-  const pendingUser = req.body;
-  const role = req.params;
 
-  Users.findByName(pendingUser.username, role)
-    .then(user =>{
-      if (user && bcrypt.compareSync(pendingUser.password, user.password)){
-        const token = makeJwt(user);
-        res.status(200).json({message:'Login successful, welcome!', token});
-      }else{
-        res.status(401).json({message:'invalid credentials'});
-      }
-      // res.status(200).json(user);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({message:'Error logging in user', error:err.message, name:err.name});
-    });
-});
+
+
+
+
+
+
+// ////OLD--------------------------------------------
+
+// router.post('/login/:role',(req,res)=>{
+//   const pendingUser = req.body;
+//   const role = req.params;
+
+//   Users.findByName(pendingUser.username, role)
+//     .then(user =>{
+//       if (user && bcrypt.compareSync(pendingUser.password, user.password)){
+//         const token = makeJwt(user);
+//         res.status(200).json({message:'Login successful, welcome!', token});
+//       }else{
+//         res.status(401).json({message:'invalid credentials'});
+//       }
+//       // res.status(200).json(user);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({message:'Error logging in user', error:err.message, name:err.name});
+//     });
+// });
+// ////OLD--------------------------------------------
+
 
 
 // router.post('/register',(req,res)=>{
--- api/users/usersModel.js
+++ api/users/usersModel.js
@@ -1,81 +1,113 @@
 const db = require('../../data/dbConfig');
 
+
 module.exports={
-  getAll,
-  findById,
-  addNew,
-  findByName
+  getAllDiners,
+  getAllOperators
+}
 
-};
-async function getAll(role){
-  try {
-    return db(role);
+async function getAllDiners(){
+  try{
+    return db('diners');
   }catch(err){
     console.log(err)
     return err;
   }
+}
+
+async function getAllOperators(){
+  // try{
+  //   return db('operators');
+  // }catch(err){
+  //   console.log(err)
+  //   return err;
+  // }
+  console.log('Test success')
+  return Promise.resolve('getAllOperators successful')
+}
+
+// ////OLD--------------------------------------------
+//----------------------------------------------------
+//----------------------------------------------------
+
+// const db = require('../../data/dbConfig');
+
+// module.exports={
+//   getAll,
+//   findById,
+//   addNew,
+//   findByName
+
+// };
+// async function getAll(role){
+//   try {
+//     return db(role);
+//   }catch(err){
+//     console.log(err)
+//     return err;
+//   }
   
-  // console.log('UsersModel test successful');
-  // return 'UsersModel test successful';
-}
-
-// ----- findById(id, role)
-// ----- requires user id, user role diner/operator
-
-async function findById(id, role){
-  try {
-    return db(role).where(id).first();
-
-  }catch(err){
-    console.log(err)
-    return err;
-  }
-}
-// async function findById(id){
-//   return db('diners')
-//   .where('diners.id', id)
-//   .first()
-//   ;
+//   // console.log('UsersModel test successful');
+//   // return 'UsersModel test successful';
 // }
 
-// ----- addNew(user)
-// ----- requires all user fields
-async function addNew(user, role){
-  // const [id] = await db(role)
-  //   .insert(user)
-  //   ;
-  // return db(role)
-  //   .where(id)
-  //   .first()
-  //   ;
-  try {
-    const [id] = await db(role)
-      .insert(user);
-      try{
-        return findById(id);
-      }catch(err){
-        console.log(err)
-        throw 'Error: Users.addNew';
-      }
-  } catch (err) {
-    console.log(err)
-    throw 'Error: Users.addNew';
-  }
-}
+// // ----- findById(id, role)
+// // ----- requires user id, user role diner/operator
 
-// ----- findByName(username, role)
-// ----- requires username, user role diner/operator
-async function findByName(username, role){
+// async function findById(id, role){
+//   try {
+//     return db(role).where(id).first();
 
-  try {
-    return db(role)
-    .where(username)
-    .first()
-    ;
+//   }catch(err){
+//     console.log(err)
+//     return err;
+//   }
+// }
+// // async function findById(id){
+// //   return db('diners')
+// //   .where('diners.id', id)
+// //   .first()
+// //   ;
+// // }
 
-  }catch(err){
-    console.log(err)
-    throw 'Error: Users.addNew';
-  }
-}
+// // ----- addNew(user)
+// // ----- requires all user fields
+// async function addNew(user, role){
+//   // const [id] = await db(role)
+//   //   .insert(user)
+//   //   ;
+//   // return db(role)
+//   //   .where(id)
+//   //   .first()
+//   //   ;
+//   try {
+//     const [id] = await db(role)
+//       .insert(user);
+//       try{
+//         return findById(id);
+//       }catch(err){
+//         console.log(err)
+//         throw 'Error: Users.addNew';
+//       }
+//   } catch (err) {
+//     console.log(err)
+//     throw 'Error: Users.addNew';
+//   }
+// }
+
+// // ----- findByName(username, role)
+// // ----- requires username, user role diner/operator
+// async function findByName(username, role){
+
+//   try {
+//     return db(role)
+//     .where(username)
+//     .first()
+//     ;
+
+//   }catch(err){
+//     console.log(err)
+//     throw 'Error: Users.addNew';
+//   }
+// }
 
